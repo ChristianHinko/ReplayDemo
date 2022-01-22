15acb6c5ee8d5a8ac340f6251d670e109179ca95
Christian Hinkle <c4hinkle@gmail.com>
2022-01-22 09:29:12 -0500

    make default Third Person CPP template

---
+++ Source/ReplayDemo/ReplayDemo.cpp
@@ -0,0 +1,7 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "ReplayDemo.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, ReplayDemo, "ReplayDemo" );
+ 
